5b21cebabf791decb5a1475c51602ada591472bb
Luiz Henrique Batista <fa34silva@gmail.com>
2024-07-03 22:54:27 -0300

    initial-settings

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	port := flag.String("p", "8100", "server-port")
+	directory := flag.String("d", ".", "the directory of static file host")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+	log.Printf("server listening %s on HTTP port : %s \n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+
+}
